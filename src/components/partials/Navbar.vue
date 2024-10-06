4c3e40a9659491610ee2412fef097051cfdf301e
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-10-06 14:57:21 +0200

    feat: add layouts pages home and jobs + navbar

---
+++ src/components/partials/Navbar.vue
@@ -0,0 +1,50 @@
+<script>
+
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<template>
+    <div class="container">
+        <ul>
+           <li><router-link :to="{name:'home'}">Home</router-link></li>
+           <li><router-link :to="{name:'job'}">I miei lavori</router-link></li>
+           
+        </ul>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.container{
+    margin: 10px 0;
+    ul{
+        list-style: none;
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+
+        li{
+            
+            text-transform: uppercase;
+           
+            a{
+                text-decoration: none;
+                color: white;
+                border: 1px solid white;
+                padding: 5px;
+                border-radius: 10px;
+                &:hover,
+                &.active{
+                    color: violet;
+                    border-color: violet;
+                    
+                }
+                
+            }
+        }
+    }
+    }
+</style>
---
+++ src/pages/Job.vue
@@ -0,0 +1,22 @@
+<script>
+    export default {
+        name: 'Job'
+    }
+</script>
+
+
+<template>
+    <div>
+        <h1>I miei progetti</h1>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, corporis. Sed labore officiis in placeat sequi cum eius ex odio quas, eum tempora repudiandae aliquid!</p>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+  p{
+    margin: 10px 0;
+  }
+</style>
